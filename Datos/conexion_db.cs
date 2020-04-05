6ddc183f7ae2bc4059b24e072d52e6f2b352e304
nclaros97 <nilsonvcm@gmail.com>
2020-04-04 20:54:38 -0600

    Prueba Farsiman commit 2

--- Datos/conexion_db.cs
+++ Datos/conexion_db.cs
@@ -11,14 +11,17 @@ namespace Datos
         public static SqlConnection conexion = null;
         public static SqlConnection getConnection()
         {
-            string servidor = "DESKTOP-LHQFM55";
+            string servidor = "127.0.0.1";
             string database = "db_pruebaFarsiman";
-            string usuario = "sa";
-            string pssw = "N1lsonClaros";
+            string usuario = "";
+            string pssw = "";
             try
             {
+                //conexion = new SqlConnection($"Data Source={servidor};Integrated " +
+                //$"Security=False;Initial Catalog={database};User Id={usuario};Password={pssw};");
+
                 conexion = new SqlConnection($"Data Source={servidor};Integrated " +
-                $"Security=False;Initial Catalog={database};User Id={usuario};Password={pssw};");
+                $"Security=True;Initial Catalog={database};");
                 conexion.Open();
 
             }
